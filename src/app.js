4b735b9ff5703e08e5ce55969dd8c07fd31491ac
demoraes <gabrielmoraes_2013@outlook.com>
2020-11-03 00:01:24 -0400

    create routes: put, delete, create and get

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,19 +11,68 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  const { techs } = request.query;
+
+  const results = techs
+    ? repositories.filter(repositorie => repositorie.techs.includes(techs))
+    : repositories;
+
+  return response.send(results);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url } = request.body;
+
+  const { techs } = request.query;
+
+  const repositorie = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repositorie);
+
+  return response.send(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { techs } = request.query;
+  const { title, url } = request.body;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' })
+  }
+
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+  };
+
+  repositories[repositorieIndex] = repositorie;
+
+  return response.send(repositorie);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' })
+  }
+
+  repositories.splice(repositorieIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
